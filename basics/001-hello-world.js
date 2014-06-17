89cde9d8dede03a55081ce2092db22a24f4e0ab1
thepeg <marco.pegoraro@gmail.com>
2014-06-17 19:13:14 +0200

    first commit

---
+++ basics/001-hello-world.js
@@ -0,0 +1,12 @@
+/**
+ * Javascript Source File
+ * ======================
+ *
+ * A JS source file like this contains pure Javascript.
+ *
+ * It does not require any special love but to be linked into
+ * a web page in order to work.
+ *
+ */
+
+console.log('Hello World from the outside!');
