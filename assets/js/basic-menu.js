93638f225a8a2ed6380311563884d4bfc01fb4b3
thepeg <marco.pegoraro@gmail.com>
2014-08-03 16:52:13 +0200

    tutorial structure in place

---
+++ assets/js/basic-menu.js
@@ -0,0 +1,16 @@
+
+(function(){
+    
+    var html =  '<h3><a href="../" title="javascript basic building blocks: variables, conditionals, functions, arrays, objects">&laquo; Basic Level <span style="font-size:0.9em">- JS Spells</span></a></h3>' +
+                '<hr><ul>' +
+                    '<li>&raquo; The Environment</li>' +
+                    '<li>&raquo; ' +
+                        '<a href="/basic/002-variables/" title="javascript variables tutorial">' +
+                        'Variables' + 
+                    '</a></li>' +
+                    '<li>&raquo; Conditionals</li>' +
+                '</ul>';
+    
+    document.write(html);
+    
+})();
