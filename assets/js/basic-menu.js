ba50ce8eaaf3507892b2ecd4eb0ae1b3b05b6290
thepeg <marco.pegoraro@gmail.com>
2014-08-09 08:02:54 +0200

    environment, hello-world, variables

--- assets/js/basic-menu.js
+++ assets/js/basic-menu.js
@@ -3,9 +3,20 @@
     
     var html =  '<h3><a href="../" title="javascript basic building blocks: variables, conditionals, functions, arrays, objects">&laquo; Basic Level <span style="font-size:0.9em">- JS Spells</span></a></h3>' +
                 '<hr><ul>' +
-                    '<li>&raquo; The Environment</li>' +
                     '<li>&raquo; ' +
-                        '<a href="/basic/002-variables/" title="javascript variables tutorial">' +
+                        '<a href="/basic/001-environment/" title="javascript environment">' +
+                        'The Environment' + 
+                    '</a></li>' +
+                    '<li>&raquo; ' +
+                        '<a href="/basic/002-hello-world/" title="javascript hello world">' +
+                        'Hello World' + 
+                    '</a></li>' +
+                    '<li>&raquo; ' +
+                        '<a href="/basic/003-expressions/" title="javascript code expressions">' +
+                        'Code Expressions' + 
+                    '</a></li>' +
+                    '<li>&raquo; ' +
+                        '<a href="/basic/004-variables/" title="javascript variables tutorial">' +
                         'Variables' + 
                     '</a></li>' +
                     '<li>&raquo; Conditionals</li>' +
